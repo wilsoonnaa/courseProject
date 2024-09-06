7dfad13d14f8892b1493c88a894a755b19572627
wilsoonnaa <avorobiovwilson@gmail.com>
2024-09-06 16:42:07 +0300

    Add responsive navbar and layout
    Add responsive navbar and responsive header, content, footer layout.

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function showSidebar(){
     const sidebar = document.querySelector('.nv-sidebar');
     sidebar.style.display = 'flex';
 }
+
+function closeSidebar(){
+    const sidebar = document.querySelector('.nv-sidebar');
+    sidebar.style.display = 'none';
+}
